e153845da1803aeae0d2e0454ca14fcab375fda3
Maria Luiza <118990550+MariaLu25@users.noreply.github.com>
2022-12-04 13:11:29 -0300

    Add files via upload

---
+++ ConversãoC_F.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (){
+    
+    float f,c,res;
+    scanf("%f",&c);
+    res = (c*1.8)+32;
+    
+    printf("%.2f",res);
+    
+}
---
+++ Media2num.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (){
+    
+    float a,b,m;
+    
+    scanf("%f %f",&a,&b);
+    m = (a + b)/2;
+    printf("%f",m);
+}
